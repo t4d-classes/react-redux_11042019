a9e4cbf6f6b9efd28372e6f8d7b1398c8cfa37b3
Eric Greene <eric@t4d.io>
2019-11-04 15:55:11 -0700

    add code demos

--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -1,19 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-export const ColorTool = () => {
+export const ColorTool = (props) => {
 
-  const colors = [
-    'blue','red','hot pink',
-    'yellow','maroon','salmon',
-  ];
+  // const [ colors, setColors ] = useState([ ...props.colors ]);
+  const [ colors, setColors ] = useState(props.colors.concat());
+
+  const [ colorForm, setColorForm ] = useState({
+    name: '',
+    hexcode: '',
+  });
+
+  const change = (e) => {
+    setColorForm({
+      ...colorForm,
+      [ e.target.name ]: e.target.value,
+    });
+  };
+
+  const addColor = (e) => {
+    e.preventDefault();
+
+    setColors(colors.concat(colorForm.name));
+
+    setColorForm({
+      name: '',
+      hexCode: '',
+    });
+  }
+
+  console.log(colorForm);
 
   return <>
     <header>
       <h1>Color Tool</h1>
     </header>
     <ul>
-      {colors.map(color => <li key={color}>{color}</li>)}
+      {colors.map( (color, index) => <li key={index} {...props}>{color}</li>)}
     </ul>
+    <form onSubmit={addColor}>
+      <div>
+        <label htmlFor="name-input">Name</label>
+        <input type="text" id="name-input"
+          name="name" value={colorForm.name} onChange={change}  />
+      </div>
+      <div>
+        <label htmlFor="hexcode-input">Hexcode</label>
+        <input type="text" id="hexcode-input"
+          name="hexcode" value={colorForm.hexcode} onChange={change}  />
+      </div>
+      <button>Add Color</button>
+    </form>
   </>;
 
+};
+
+ColorTool.defaultProps = {
+  colors: [],
+};
+
+ColorTool.propTypes = {
+  colors: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 
 import { ColorTool } from './components/ColorTool';
 
+const colorList = [
+  'blue','red','hot pink',
+  'yellow','maroon','salmon',
+];
+
 ReactDOM.render(
-  <ColorTool />,
+  <>
+    <ColorTool colors={colorList} />
+  </>,
   document.querySelector('#root'),
 );
