09b6d8b9ef8e00345f6922cdaeb4244ddf18cd14
Eric Greene <eric@t4d.io>
2019-11-06 06:41:19 -0700

    add code demos

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -1,6 +1,10 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
 const ADD_ACTION = 'ADD';
 const SUBTRACT_ACTION = 'SUBTRACT';
-
+const MULTIPLY_ACTION = 'MULTIPLY';
+const DIVIDE_ACTION = 'DIVIDE';
 
 const calcToolReducer = (state = 0, action) => {
   switch (action.type) {
@@ -8,6 +12,10 @@ const calcToolReducer = (state = 0, action) => {
       return state + action.value;
     case SUBTRACT_ACTION:
       return state - action.value;
+    case MULTIPLY_ACTION:
+      return state * action.value;
+    case DIVIDE_ACTION:
+      return state / action.value;
     default:
       return state;
   }
@@ -28,17 +36,47 @@ const createStore = (reducerFn) => {
   };
 };
 
+const createAddAction = value => ({ type: ADD_ACTION, value });
+const createSubtractAction = value => ({ type: SUBTRACT_ACTION, value });
+const createMultiplyAction = value => ({ type: MULTIPLY_ACTION, value });
+const createDivideAction = value => ({ type: DIVIDE_ACTION, value });
+
+const Calculator = ({ result, onAdd, onSubtract, onMultiply, onDivide }) => {
+
+  const [ num, setNum ] = useState(0);
+
+  return <form>
+    <div>
+      Result: {result}
+    </div>
+    <div>
+      <label>Num Input:</label>
+      <input type="number" value={num}
+        onChange={({ target: { value }}) => setNum(Number(value))} />
+    </div>
+    <div>
+      <button type="button" onClick={() => onAdd(num)}>Add</button>
+      <button type="button" onClick={() => onSubtract(num)}>Subtract</button>
+      <button type="button" onClick={() => onMultiply(num)}>Multiply</button>
+      <button type="button" onClick={() => onDivide(num)}>Divide</button>
+    </div>
+  </form>;
+};
+
 const store = createStore(calcToolReducer);
 
 store.subscribe(() => {
-  console.log(store.getState());
+  ReactDOM.render(
+    <Calculator
+      result={store.getState()}
+      onAdd={num => store.dispatch(createAddAction(num))}
+      onSubtract={num => store.dispatch(createSubtractAction(num))}
+      onMultiply={num => store.dispatch(createMultiplyAction(num))}
+      onDivide={num => store.dispatch(createDivideAction(num))}
+    />,
+    document.querySelector('#root'),
+  );
 });
 
-const createAddAction = value => ({ type: ADD_ACTION, value });
-const createSubtractAction = value => ({ type: SUBTRACT_ACTION, value });
 
-store.dispatch(createAddAction(1));
-store.dispatch(createSubtractAction(2));
-store.dispatch(createAddAction(3));
-store.dispatch(createSubtractAction(4));
-store.dispatch(createAddAction(5));
+store.dispatch(createAddAction(0));
