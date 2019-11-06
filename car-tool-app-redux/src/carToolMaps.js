d0c9bd9260dc3d1e61025bd02aa6d0c27c2a91ba
Eric Greene <eric@t4d.io>
2019-11-06 16:37:30 -0700

    add code demos

--- car-tool-app-redux/src/carToolMaps.js
+++ car-tool-app-redux/src/carToolMaps.js
@@ -1,8 +1,8 @@
 import { bindActionCreators } from 'redux';
 
 import {
-  createAppendCarAction, createReplaceCarAction,
-  createEditCarAction,
+  createAppendCarAction, createReplaceCarAction, createDeleteCarAction,
+  createEditCarAction, createCancelCarAction
 } from './carToolActions';
 
 export const mapStateToProps = ({ cars, editCarId }) => ({ cars, editCarId });
@@ -10,5 +10,7 @@ export const mapStateToProps = ({ cars, editCarId }) => ({ cars, editCarId });
 export const mapDispatchToProps = dispatch => bindActionCreators({
   appendCar: createAppendCarAction,
   replaceCar: createReplaceCarAction,
+  deleteCar: createDeleteCarAction,
   editCar: createEditCarAction,
+  cancelCar: createCancelCarAction,
 }, dispatch);
